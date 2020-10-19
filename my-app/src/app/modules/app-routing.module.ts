c4d444326e0e20c035b80e7f6a80f99cb3a68549
Tylyn Adams <tyadams73@gmail.com>
2020-10-19 07:29:33 -0600

    started project

--- my-app/src/app/modules/app-routing.module.ts
+++ my-app/src/app/modules/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { api } from 'https://golf-courses-api.herokuapp.com/courses'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
